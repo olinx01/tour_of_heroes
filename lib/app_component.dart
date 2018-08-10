f8c7395a6c32bb5fadb1cd565a0f17a898a484c0
olin <olinx01@gmail.com>
2018-08-10 11:35:39 +0200

    ep. 1

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,11 +1,23 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import 'src/hero/hero.dart';
+import 'src/hero/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
+  styleUrls: [ 'app_component.css' ],
+  directives: [ coreDirectives, formDirectives ]
 )
 class AppComponent {
 
-  String greeting = "Hi!";
+  String title = "Tour of Heroes";
+
+  List<Hero> heroes = mock_heroes;
+
+  Hero selected;
+
+  void onSelect(Hero hero) => this.selected = hero;
 
 }
--- test/app_test.dart
+++ test/app_test.dart
@@ -1,7 +1,7 @@
 @TestOn('browser')
 
-import 'package:angular_app/app_component.dart';
-import 'package:angular_app/app_component.template.dart' as ng;
+import 'package:tour_of_heroes/app_component.dart';
+import 'package:tour_of_heroes/app_component.template.dart' as ng;
 import 'package:angular_test/angular_test.dart';
 import 'package:test/test.dart';
 
@@ -21,8 +21,8 @@ void main() {
   });
 
   test('Greet world', () async {
-    await fixture.update((c) => c.greeting = 'World!');
-    expect(fixture.text, 'World! This is Angular!');
+    await fixture.update((c) => c.title = 'Tour of Heroes');
+    expect(fixture.text, 'Tour of Heroes');
   });
 
   test('Greet world HTML', () {
